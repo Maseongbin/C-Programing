1943d76d90e602ed56741290bde1d0d92fc2a4b3
Maseongbin <125714011+Maseongbin@users.noreply.github.com>
2023-04-11 18:53:23 +0900

    Add files via upload

---
+++ C언어 도전 150제/20230410/20230410_1/20230410_1/main.cpp
@@ -0,0 +1,22 @@
+
+#include<stdio.h>
+
+
+int main(void)
+{
+	int x = 1;
+	int y = 2;
+	int z = x + y;
+	printf("변수 x의 값을 입력하시오: ");
+	printf("%d\n", x);
+
+	printf("변수 y의 값을 입력하시오: ");
+	printf("%d\n", y);
+
+	printf("z의 값을 입력하시오: ");
+	printf("%d\n", z);
+
+	return 0;
+
+
+}
