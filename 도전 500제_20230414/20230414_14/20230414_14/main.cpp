78859d26f8aa999a9add4344d649e6bda410b3b4
Maseongbin <125714011+Maseongbin@users.noreply.github.com>
2023-04-16 14:14:11 +0900

    Add files via upload

---
+++ 도전 500제_20230414/20230414_14/20230414_14/main.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+int main(void)
+{
+
+	bool b;
+
+	b = 5 < 10;
+
+	printf("%d\n", b);
+
+	return 0;
+
+}
+//논리형 변수에서 true는 정수 1, false는 정수 0 b가 true이기에 1이 나온다.
---
+++ 도전 500제_20230414/20230414_15/20230414_15/main.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(void)
+{
+
+
+	char str[] = "¥Î«—πŒ±π";
+
+	printf("str: %s\n", str);
+
+	return 0;
+}
