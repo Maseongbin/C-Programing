4dd65518d8bb274f65528828dd35d43e4400f0bf
Maseongbin <125714011+Maseongbin@users.noreply.github.com>
2023-11-02 15:26:28 +0900

    Add files via upload

---
+++ 2023년 2학기/만도 C프로그래밍/문자열/20231102/20231102/main.cpp
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+
+/*
+int main()
+{
+    char s1[10];   
+
+    printf("문자열을 입력하세요: ");
+    scanf("%s", s1);     
+
+    printf("%s\n", s1);  
+
+    return 0;
+}
+*/
+
+int main()
+{
+    char s1[30];
+
+    printf("문자열을 입력하세요: ");
+    scanf("%[^\n]s", s1);   
+
+    printf("%s\n", s1);
+}
