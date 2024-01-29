4b666b8aa8a0913ed8b32506f6473b0d6a575c02
Maseongbin <125714011+Maseongbin@users.noreply.github.com>
2024-01-29 16:34:41 +0900

    Add files via upload

---
+++ 2023년 2학기/겨울방학/파일 입출력/20240129/20240129/main.cpp
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define MAX_LEN 100
+#include <stdio.h>
+
+/*
+// 파일 출력
+int main()
+{
+    FILE* fs;
+    fs = fopen("test1.txt", "w");
+    fputs("안녕하세요. 마성빈입니다.", fs);
+    fclose(fs);
+}
+*/
+
+/*
+//  파일 입력
+int main()
+{
+    char str[100];
+    FILE* fs;
+    fs = fopen("test2.txt", "r");
+    fscanf(fs ,"%s", str);
+    printf("%s \n", str);
+    fclose(fs);
+}
+*/
